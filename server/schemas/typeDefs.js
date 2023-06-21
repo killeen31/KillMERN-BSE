5c295609b6ab8fbd00f0fcf38041b8667ef41073
killeen31 <j31killeen@gmail.com>
2023-06-21 12:50:00 -0400

    added typeDefs.js and index.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+    type mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+
